a3ae77279071341bdd00de7624f053e225c7692a
Hyunhae Lee <hyunhae.lee@gmail.com>
2020-04-09 14:39:53 +0000

    [asn1] add byte to hex string
    add byte to hex string and test function

--- src/main/kotlin/hyunh/asn1/Utils.kt
+++ src/main/kotlin/hyunh/asn1/Utils.kt
@@ -1,11 +1,19 @@
 package hyunh.asn1
 
-fun ByteArray.toHexString() : String {
-    val hex = "0123456789ABCDEF"
-    val builder = StringBuilder(this.size * 2);
-    forEach {
-        builder.append(hex[it.toInt().ushr(4) and 0x0F])
-        builder.append(hex[it.toInt() and 0x0F])
-    }
-    return builder.toString()
+fun ByteArray.toHexString(): String {
+    return StringBuilder(this.size * 2).also { builder ->
+        val hex = "0123456789ABCDEF"
+        forEach {
+            builder.append(hex[it.toInt().ushr(4) and 0x0F])
+            builder.append(hex[it.toInt() and 0x0F])
+        }
+    }.toString()
+}
+
+fun Byte.toHexString(): String {
+    return StringBuilder().apply {
+        val hex = "0123456789ABCDEF"
+        append(hex[toInt().ushr(4) and 0x0F])
+        append(hex[toInt() and 0x0F])
+    }.toString()
 }
--- src/test/kotlin/hyunh/asn1/UtilsTest.kt
+++ src/test/kotlin/hyunh/asn1/UtilsTest.kt
@@ -5,9 +5,15 @@ import org.junit.Test
 
 class UtilsTest {
     @Test
-    fun toHexStringTest() {
+    fun byteArrayToHexStringTest() {
         val actual = byteArrayOf(0x12, 0x34, 0x56, 0x78, 0x9A.toByte()
             , 0xBC.toByte(), 0xDE.toByte(), 0xFF.toByte())
         Assert.assertEquals("123456789ABCDEFF", actual.toHexString())
+    }
+
+    @Test
+    fun byteToHexStringTest() {
+        val actual = 0xFF.toByte()
+        Assert.assertEquals("FF", actual.toHexString())
     }
 }
